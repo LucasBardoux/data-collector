b9ce18c24bbea146d1c0b15257dd9801e62d525a
LucasBardoux <bardoux.l@gmx.de>
2026-03-03 00:41:54 +0100

    feat: add health check

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use('/device-location', deviceLocationRouter);
 
 connect(process.env.MONGODB_URI!, {
   dbName: process.env.DB_NAME,
-}).then(async () => {
+}).then(() => {
+  app.get('/health', (_, res) => res.send({ status: 'OK' }));
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
